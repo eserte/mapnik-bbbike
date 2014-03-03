2137c8b1153b677f59de446543b7f599cc53d78d
Slaven Rezic <slaven@rezic.de>
2014-03-03 23:46:42 +0100

    renderer.py: more mapfile and bbox samples

--- tools/renderer.py
+++ tools/renderer.py
@@ -8,7 +8,14 @@ from mapnik import *
 #mapfile = '../../../work/mapnik-german/osm-de2.xml'
 #mapfile = '../../../work/mapnik-german/bbbike-smoothness.xml'
 #mapfile = '../../../work/mapnik-german/bbbike-handicap.xml'
+
 mapfile = '../bbbike.xml'
+#mapfile = '../bbbike-smoothness.xml'
+#mapfile = '../bbbike-smoothness-solid.xml'
+#mapfile = '../bbbike-handicap.xml'
+#mapfile = '../bbbike-cycleway.xml'
+#mapfile = '../bbbike-cycle-routes.xml'
+
 map_output = '/tmp/mymap.png'
 
 m = Map(1024,768)
@@ -18,6 +25,8 @@ load_map(m, mapfile)
 #bbox=(Envelope( -85,-170,85,170 ))
 #bbox=(Envelope(1481888.238355513,6883816.509324187,1495738.040636882,6896019.182691022));
 bbox=(Envelope(1486304.184101673,6886729.462978764,1494880.051892038,6893551.420543496));
+#bbox=(Envelope(1086304.184101673,6386729.462978764,2094880.051892038,7393551.420543496));
+#bbox=(Envelope(486304.184101673,5886729.462978764,2494880.051892038,7893551.420543496));
 
 m.zoom_to_box(bbox)
 print "Scale = " , m.scale()
