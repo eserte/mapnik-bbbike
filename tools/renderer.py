a36c8c3d4ade010872e368e15fe9e76f0f9dbbbc
Slaven Rezic <slaven@rezic.de>
2014-03-06 20:32:59 +0100

    renderer.py: new option --bbox

--- tools/renderer.py
+++ tools/renderer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# The projection stuff taken from
+# http://svn.openstreetmap.org/applications/rendering/mapnik/generate_image.py
+
 try:
     import mapnik
 except:
@@ -19,6 +22,8 @@ parser.add_argument('--view', action='store_true',
                     help='display file immediately with ImageMagick')
 parser.add_argument('--geometry', default="1024x768",
                     help='size of image in the form widthxheight (default: 1024x768)')
+parser.add_argument('--bbox', default="13.338273,52.535687,13.451261,52.499058",
+                    help='coordinates of the visible map in the form minlon,minlat,maxlon,maxlat (default: center of Berlin)')
 args = parser.parse_args()
 
 mapfile = rootdir + '/' + args.mapfile + '.xml'
@@ -28,14 +33,38 @@ w = int(w)
 h = int(h)
 m = mapnik.Map(w,h)
 
+if args.bbox == None:
+    args.bbox = "13.338273,52.535687,13.451261,52.499058"
+bounds = map(lambda x: float(x), args.bbox.split(','))
+
+merc = mapnik.Projection('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over')
+longlat = mapnik.Projection('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
 mapnik.load_map(m, mapfile)
-#bbox=(Envelope( 13.432570,52.504220, 13.458203,52.516270 ))
-#bbox=(Envelope( 52.516270,13.432570, 52.504220,13.458203 ))
-#bbox=(Envelope( -85,-170,85,170 ))
-#bbox=(Envelope(1481888.238355513,6883816.509324187,1495738.040636882,6896019.182691022));
-bbox=(mapnik.Envelope(1486304.184101673,6886729.462978764,1494880.051892038,6893551.420543496));
-#bbox=(Envelope(1086304.184101673,6386729.462978764,2094880.051892038,7393551.420543496));
-#bbox=(Envelope(486304.184101673,5886729.462978764,2494880.051892038,7893551.420543496));
+
+# ensure the target map projection is mercator
+m.srs = merc.params()
+
+if hasattr(mapnik,'Box2d'):
+    bbox = mapnik.Box2d(*bounds)
+else:
+    bbox = mapnik.Envelope(*bounds)
+
+# Our bounds above are in long/lat, but our map
+# is in spherical mercator, so we need to transform
+# the bounding box to mercator to properly position
+# the Map when we call `zoom_to_box()`
+transform = mapnik.ProjTransform(longlat,merc)
+merc_bbox = transform.forward(bbox)
+
+# Mapnik internally will fix the aspect ratio of the bounding box
+# to match the aspect ratio of the target image width and height
+# This behavior is controlled by setting the `m.aspect_fix_mode`
+# and defaults to GROW_BBOX, but you can also change it to alter
+# the target image size by setting aspect_fix_mode to GROW_CANVAS
+#m.aspect_fix_mode = mapnik.GROW_CANVAS
+# Note: aspect_fix_mode is only available in Mapnik >= 0.6.0
+m.zoom_to_box(merc_bbox)
 
 if args.view:
     import tempfile
@@ -47,7 +76,6 @@ elif args.outfile == None:
 else:
     map_output = args.outfile
 
-m.zoom_to_box(bbox)
 print "Scale = " , m.scale()
 mapnik.render_to_file(m, map_output)
 
